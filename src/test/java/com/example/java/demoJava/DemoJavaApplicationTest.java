142cd2548982907de7627304b3ed82c4ee6c48cc
sneha-paul <sneha.paul@e-xposures.com>
2021-11-23 21:06:57 +0530

    Added my project2

---
+++ src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
@@ -0,0 +1,28 @@
+package com.example.java.demoJava;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoJavaApplicationTest {
+	@Test
+	public void contextLoads() throws InterruptedException
+	{
+
+		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome96\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		String actualUrl="https://www.amazon.com/";
+		String expectedUrl= driver.getCurrentUrl();
+		Assert.assertEquals(expectedUrl,actualUrl);
+
+	}
+
+}
+
+
+

de9b779aa44fbac54f437616233bcf9845d24ed8
sneha-paul <59996996+sneha-paul@users.noreply.github.com>
2022-07-04 22:01:49 +0530

    Update DemoJavaApplicationTest.java

--- src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
+++ src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
@@ -10,18 +10,16 @@ public class DemoJavaApplicationTest {
 	@Test
 	public void contextLoads() throws InterruptedException
 	{
-
 		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome102\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		Thread.sleep(2000);
-		
 		String actualUrl="https://www.amazon.com/";
 		String expectedUrl= driver.getCurrentUrl();
 		Assert.assertEquals(expectedUrl,actualUrl);
 		Thread.sleep(2000);
-        driver.quit();
+         driver.quit();
 	}
 
 }

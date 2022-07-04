d24779a9936d19ff7a9d1abb5bcb58cf5c75e73d
sneha-paul <59996996+sneha-paul@users.noreply.github.com>
2022-07-04 21:54:39 +0530

    Update DemoJavaApplicationTest.java

--- src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
+++ src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
@@ -16,7 +16,6 @@ public class DemoJavaApplicationTest {
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		Thread.sleep(2000);
-		
 		String actualUrl="https://www.amazon.coms/";
 		String expectedUrl= driver.getCurrentUrl();
 		Assert.assertEquals(expectedUrl,actualUrl);

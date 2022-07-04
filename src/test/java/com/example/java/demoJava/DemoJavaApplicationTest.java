286e57a10ff137cc64c470c11e732fe6e76264f0
sneha-paul <sneha.paul@e-xposures.com>
2022-07-04 20:56:12 +0530

    testng code update

--- src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
+++ src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
@@ -11,7 +11,7 @@ public class DemoJavaApplicationTest {
 	public void contextLoads() throws InterruptedException
 	{
 
-		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome96\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome102\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");

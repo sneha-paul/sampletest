e7c0ec56ad30e70d0911af9baa24ea40c9fef792
sneha-paul <59996996+sneha-paul@users.noreply.github.com>
2022-07-05 01:41:55 +0530

    Update DemoJavaApplicationTest.java

--- src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
+++ src/test/java/com/example/java/demoJava/DemoJavaApplicationTest.java
@@ -13,7 +13,7 @@ public class DemoJavaApplicationTest {
 		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome102\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.amazon.coms/");
+		driver.get("https://www.amazon.com/");
 		Thread.sleep(2000);
 		String actualUrl="https://www.amazon.com/";
 		String expectedUrl= driver.getCurrentUrl();
